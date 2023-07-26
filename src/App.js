ae7b3791dfffdee06ebb5e21836ba0743d662b9a
Bhanuprakash-340 <bhanu150940@gmail.com>
2023-07-26 13:13:12 +0530

    content changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () =>{
     return (
         <h1>
-            Hello world! Bhanu's using React
+            Hello world! Bhanu's using React developer sneha
         </h1>
     )
 }
